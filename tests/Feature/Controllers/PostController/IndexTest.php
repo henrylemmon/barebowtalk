43e2eab827b728f39045228387463aa8bb569b01
Henry Lemmon <henrylemmon@Henrys-MBP.frontierlocal.net>
2024-08-21 19:53:31 -0700

    update indextest to use helper

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -1,23 +1,17 @@
 <?php
 
-use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\Json\ResourceCollection;
 use function Pest\Laravel\get;
-use Inertia\Testing\AssertableInertia;
+use App\Http\Resources\PostResource;
 
 it('should return the posts index component', function() {
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->component('Posts/Index', true)
-        );
+        ->assertComponent('Posts/Index');
 });
 
 it('passes posts to the posts.index view', function() {
     $posts = Post::factory(3)->create();
 
     get(route('posts.index'))
-        /*->assertHasResource('post', PostResource::make($posts->first()))*/
         ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
 });
