dc4dea32306d775294f8e3c96a51b0b923a40611
Henry Lemmon <henrylemmon@Henrys-MBP.frontierlocal.net>
2024-08-27 16:54:07 -0700

    tested for correct redirect route

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -9,6 +9,11 @@ it('should return the posts index component', function() {
         ->assertComponent('Posts/Index');
 });
 
+it('root url should return the posts.index route', function() {
+    get('/')
+        ->assertRedirect(route('posts.index'));
+});
+
 it('passes posts to the posts.index view', function() {
     $posts = Post::factory(3)->create();
 
