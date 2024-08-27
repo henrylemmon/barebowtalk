e28151c7edc74ed0311a470b7ca8299377782a8e
Henry Lemmon <henrylemmon@Henrys-MBP.frontierlocal.net>
2024-08-27 16:04:45 -0700

    redirect / from welcome to posts

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+});*/
+
+Route::redirect('/', 'posts');
 
 Route::middleware([
     'auth:sanctum',
